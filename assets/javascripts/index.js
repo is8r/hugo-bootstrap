dffcbbf38902d3937ee8f170e73e31d929e6904c
Yu Ishihara <ishihara.yu@gmail.com>
2020-06-24 23:52:55 +0900

    Update

---
+++ assets/javascripts/index.js
@@ -0,0 +1,9 @@
+import '../stylesheets/styles.scss';
+
+import $ from 'jquery'
+import bootstrap from 'bootstrap'
+import HugoSearch from './classes/HugoSearch'
+
+$(function() {
+  var hugoSearch = new HugoSearch()
+});
---
+++ webpack/base.config.js
@@ -0,0 +1,50 @@
+const path = require('path');
+// const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  context: path.resolve(__dirname, '../assets'),
+  entry: {
+    app: ['./javascripts/index.js']
+  },
+  output: {
+    path: path.resolve(__dirname, '../static')
+  },
+  // devServer: {
+  //   contentBase: path.join(__dirname, '../static'),
+  //   watchContentBase: true
+  // },
+  module: {
+    rules: [
+      {
+        test: /\.(css|scss)$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          {
+            loader: 'postcss-loader'
+          },
+          {
+            loader: 'sass-loader'
+          }
+        ]
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: 'babel-loader'
+      }
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin()
+    // new HtmlWebpackPlugin({
+    //   template: './html/index.ejs',
+    //   minify: {
+    //     removeComments: true,
+    //     collapseWhitespace: true,
+    //     removeAttributeQuotes: true
+    //   }
+    // })
+  ]
+}
