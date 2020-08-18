f7834e09495545ad0cb03d8907976e50f0d40de9
Yu Ishihara <ishihara.yu@gmail.com>
2020-08-19 00:00:19 +0900

    Update

--- assets/javascripts/index.js
+++ assets/javascripts/index.js
@@ -2,32 +2,9 @@ import "../stylesheets/styles.scss";
 
 import { Modal, Dropdown } from "bootstrap";
 import HugoSearch from "./classes/HugoSearch";
+import Hamburger from "./classes/Hamburger";
 
 document.addEventListener("DOMContentLoaded", () => {
   new HugoSearch();
-  initMenu();
+  new Hamburger();
 });
-
-function initMenu() {
-  const body = document.querySelector("body");
-  const menu = document.querySelector(".menu-trigger");
-  const menuCover = document.querySelector(".menu-cover");
-  const menuInput = document.querySelector(
-    '.menu-trigger input[type="checkbox"]'
-  );
-
-  if (menu) {
-    menuCover.addEventListener("click", () => {
-      body.classList.remove("open");
-      menuInput.checked = false;
-    });
-    menu.addEventListener("click", () => {
-      if (!menuInput.checked) {
-        body.classList.remove("open");
-        menuInput.checked = false;
-      } else if (!body.classList.contains("open")) {
-        body.classList.add("open");
-      }
-    });
-  }
-}
